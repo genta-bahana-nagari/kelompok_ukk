3e9190f51cff500c82a6002ec5aff1d1a010ea40
genta-bahana-nagari <gentapossenti@gmail.com>
2025-04-23 18:44:51 +0700

    resource keluar masuk ?

---
+++ app/Filament/Resources/KeluarResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\KeluarResource\Pages;
+use App\Filament\Resources\KeluarResource\RelationManagers;
+use App\Models\BarangKeluar;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class KeluarResource extends Resource
+{
+    protected static ?string $model = BarangKeluar::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-arrow-up-tray';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListKeluars::route('/'),
+            'create' => Pages\CreateKeluar::route('/create'),
+            'edit' => Pages\EditKeluar::route('/{record}/edit'),
+        ];
+    }
+}
